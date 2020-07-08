3db2d59c0e22aa58edbbff2582430022bf85818f
NaphatB <naphat@orcsoft.co.th>
2020-07-08 17:39:50 +0700

    add custom animate route

--- lib/main.dart
+++ lib/main.dart
@@ -117,10 +117,19 @@ class _RandomWordsState extends State<RandomWords> {
 
   Route _createRoute(StatelessWidget target) {
     return PageRouteBuilder(
-        pageBuilder: (context, animation, secondaryAnimation) => target,
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return child;
-        });
+      pageBuilder: (context, animation, secondaryAnimation) => target,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(0.0, 1.0);
+        var end = Offset.zero;
+        var tween = Tween(begin: begin, end: end);
+        var offsetAnimation = animation.drive(tween);
+
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
+    );
     // return MaterialPageRoute(builder: (context) => target);
   }
 }
